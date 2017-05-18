b8cb50805d6320321bca068f504d4b326febbe73
Brandon Innis <brandoninnis@brandons-mbp.fios-router.home>
2017-05-17 22:44:27 -0400

    Initial Commit

---
+++ Weather-Forcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather-Forcast
+//
+//  Created by Brandon Innis on 5/17/17.
+//  Copyright © 2017 Brandon Innis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
